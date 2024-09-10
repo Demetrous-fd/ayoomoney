9e5c90ea5f650fbcf301c1e269da3f530e929a68
Demetrous-fd <dimaprodeus@yandex.ru>
2024-09-10 23:27:38 +0800

    Update authorization.py

--- aiomoney/authorization.py
+++ aiomoney/authorization.py
@@ -5,32 +5,34 @@ TOKEN_URL = "https://yoomoney.ru/oauth/token"
 
 
 async def authorize_app(client_id, redirect_uri, app_permissions: list[str, ...]):
+    client = AsyncClient()
+
     auth_params = dict(
         client_id=client_id,
         redirect_uri=redirect_uri,
         scope=app_permissions,
         response_type="code"
     )
+    response = await client.post(AUTH_URL, params=auth_params)
 
-    async with AsyncClient() as client:
-        response = await client.post(AUTH_URL, params=auth_params)
-    
     print(f"Перейдите по URL и подтвердите доступ для приложения\n{response.url}")
     code = input("Введите код в консоль >  ").strip()
-    
+
     token_params = dict(
         code=code,
         client_id=client_id,
         redirect_uri=redirect_uri,
         grant_type="authorization_code"
     )
-    async with AsyncClient() as client:
-        response = await client.post(TOKEN_URL, params=token_params)
+    response = await client.post(TOKEN_URL, params=token_params)
+    await client.aclose()
 
     data = response.json()
 
     access_token = data.get("access_token")
     if not access_token:
-        return print(f"Не удалось получить токен. {data.get('error', '')}")
-    
-    return print(f"Ваш токен — {access_token}. Сохраните его в безопасном месте!")
+        print(f"Не удалось получить токен. {data.get('error', '')}")
+        return
+
+    print(f"Ваш токен — {access_token}. Сохраните его в безопасном месте!")
+    return
