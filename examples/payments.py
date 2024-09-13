3ce0797c3e7d8d139949b3a658a3f5e5edc01b7b
Demetrous-fd <dimaprodeus@yandex.ru>
2024-09-14 01:06:33 +0800

    Rename package; Create sync client and cli auth tool; Update README.md

--- examples/payments.py
+++ examples/payments.py
@@ -1,26 +1,49 @@
-import asyncio
 from os import environ
 
 from dotenv import load_dotenv
 
-from aiomoney.wallet import YooMoneyWallet, PaymentSource
+from ayoomoney.wallet import YooMoneyWallet, YooMoneyWalletAsync, PaymentSource
 
 load_dotenv()
 
 
-async def main():
+def sync_example():
     wallet = YooMoneyWallet(access_token=environ.get("ACCESS_TOKEN"))
-    
-    payment_form = await wallet.create_payment_form(
-        amount_rub=10,
-        unique_label="myproject_second_unicorn",
+
+    payment_form = wallet.create_payment_form(
+        amount_rub=42,
+        unique_label="myproject",
         payment_source=PaymentSource.YOOMONEY_WALLET,
-        success_redirect_url="https://t.me/fofmow (nonono =/)"
+        success_redirect_url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xl"
+    )
+    payment_is_completed = wallet.check_payment_on_successful(payment_form.payment_label)
+    print(
+        f"Ссылка на оплату:\n{payment_form.link_for_customer}\n\n"
+        f"Форма оплачена: {'Да' if payment_is_completed else 'Нет'}"
+    )
+
+    wallet.close()
+
+
+async def async_example():
+    wallet = YooMoneyWalletAsync(access_token=environ.get("ACCESS_TOKEN"))
+
+    payment_form = await wallet.create_payment_form(
+        amount_rub=42,
+        unique_label="myproject",
+        payment_source=PaymentSource.YOOMONEY_WALLET,
+        success_redirect_url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xl"
     )
     payment_is_completed = await wallet.check_payment_on_successful(payment_form.payment_label)
-    print(f"Ссылка на оплату:\n{payment_form.link_for_customer}\n\n"
-          f"Форма оплачена: {'Да' if payment_is_completed else 'Нет'}")
+    print(
+        f"Ссылка на оплату:\n{payment_form.link_for_customer}\n\n"
+        f"Форма оплачена: {'Да' if payment_is_completed else 'Нет'}"
+    )
+
+    await wallet.close()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # sync_example()
+    import asyncio
+    asyncio.run(async_example())
--- examples/wallet_info.py
+++ examples/wallet_info.py
@@ -3,8 +3,8 @@ from os import environ
 
 from dotenv import load_dotenv
 
-from aiomoney.types import AccountInfo, Operation, OperationDetails
-from aiomoney.wallet import YooMoneyWallet
+from ayoomoney.types import AccountInfo, OperationHistory, OperationDetails
+from ayoomoney.wallet import YooMoneyWallet
 
 load_dotenv()
 
@@ -12,14 +12,14 @@ load_dotenv()
 async def main():
     wallet = YooMoneyWallet(access_token=environ.get("ACCESS_TOKEN"))
     
-    account_info: AccountInfo = await wallet.account_info
+    account_info: AccountInfo = await wallet.account_info()
     """
     account='4100116938791262' balance=small =) currency='643' account_status='identified'
     account_type='personal' balance_details=BalanceDetail(total=X, available=X,
     deposition_pending=None, blocked=None, debt=None, hold=None) cards_linked=None
     """
     
-    operation_history: list[Operation] = await wallet.get_operation_history()
+    operation_history: OperationHistory = await wallet.get_operation_history()
     """
     [Operation(operation_id='000000', status='success',
     execution_datetime=datetime.datetime(2023, 3, 9, 13, 15, 10, tzinfo=datetime.timezone.utc),
